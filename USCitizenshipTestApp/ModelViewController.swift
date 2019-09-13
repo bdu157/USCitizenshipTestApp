b353613aaf2e4be390aa644fd05cf6b66b8f5799
Dongwoo Pae <dongwoopae@gmail.com>
2019-09-13 23:23:41 +0900

    added sectionHeader with cell count and segmentedControl

--- USCitizenshipTestApp/ModelViewController.swift
+++ USCitizenshipTestApp/ModelViewController.swift
@@ -29,38 +29,4 @@ class ModelViewController {
             return
         }
     }
-    
-//    var imgURL = "https://www.dongwoopae.com/resources/img/"
-//
-//    var imgUrlArray = [String]()
-//
-//    func imageSetUp() {
-//    for i in 1..<6 {
-//        let imgURLs = imgURL + "\(i).jpg"
-//        imgUrlArray.append(imgURLs)
-//        }
-//    }
-    
-//    func fetchImages(imgUrlString: String, completion:@escaping (Result<Data, NetworkError>)-> Void) {
-//
-//        let imageUrl = URL(string: imgUrlString)!
-//
-//        var request = URLRequest(url: imageUrl)
-//        request.httpMethod = "GET"
-//
-//        URLSession.shared.dataTask(with: request) { (data, _, error) in
-//            if let _ = error {
-//                completion(.failure(.otherError))
-//                return
-//            }
-//
-//            guard let data = data else {
-//                completion(.failure(.noData))
-//                return
-//            }
-//
-//            print(data)
-//            completion(.success(data))
-//        }.resume()
-//    }
 }
---
+++ USCitizenshipTestApp/Models/Section.swift
@@ -0,0 +1,13 @@
+//
+//  Section.swift
+//  USCitizenshipTestApp
+//
+//  Created by Dongwoo Pae on 9/13/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+
+class Section {
+    var count = 0
+}
