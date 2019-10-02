c463bbd54cd1e9366c348d953a75c33e0666ed89
Dongwoo Pae <dongwoopae@gmail.com>
2019-10-03 00:33:44 +0900

    added appearanceHelper for brightmode and defaultmode for SettingsViewController

--- USCitizenshipTestApp/SettingsViewControllers/AppearanceHelper.swift
+++ USCitizenshipTestApp/SettingsViewControllers/AppearanceHelper.swift
@@ -10,7 +10,29 @@ import Foundation
 import UIKit
 
 enum AppearanceHelper {
-
-    // set up appearance based on userDefaults being saved after choosing bright mode in setting
     
+    static var mainColorDarkBlue = #colorLiteral(red: 0.0995137468, green: 0.263354212, blue: 0.4718250036, alpha: 1)
+    // set up appearance based on userDefaults being saved after choosing bright mode in setting
+    static func setDefaultAppearance() {
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().titleTextAttributes = textAttributes
+        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.0995137468, green: 0.263354212, blue: 0.4718250036, alpha: 1)
+        
+        UITabBar.appearance().barTintColor = #colorLiteral(red: 0.0995137468, green: 0.263354212, blue: 0.4718250036, alpha: 1)
+        UITabBar.appearance().tintColor = .white
+    }
+    
+    static func setWhiteAppearance() {
+        let textAttributes = [NSAttributedString.Key.foregroundColor: mainColorDarkBlue]
+        UINavigationBar.appearance().titleTextAttributes = textAttributes
+        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        
+        UINavigationBar.appearance().barTintColor = .white
+        
+        UITabBar.appearance().barTintColor = .white
+        UITabBar.appearance().tintColor = #colorLiteral(red: 0.0995137468, green: 0.263354212, blue: 0.4718250036, alpha: 1)
+    }
 }
+
